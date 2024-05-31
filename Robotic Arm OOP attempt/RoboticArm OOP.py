a2648d7ae0e43b4460bca34b03d13da44a839572
DavidGonzalez2399 <128091803+DavidGonzalez2399@users.noreply.github.com>
2024-05-31 09:34:35 +0200

    update
    update

--- Robotic Arm OOP attempt/RoboticArm OOP.py
+++ Robotic Arm OOP attempt/RoboticArm OOP.py
@@ -4,15 +4,15 @@ from motor_class import Motor
 motor1_wait_ms=20
 motor2_wait_ms=90
 motor3_wait_ms=20
-motor5_wait_ms=20
-motor6_wait_ms=1000
+motor5_wait_ms=10
+motor6_wait_ms=5 
 
 motor_wait_list=[motor1_wait_ms,motor2_wait_ms,motor3_wait_ms,motor5_wait_ms,motor6_wait_ms]
 motor1 = Motor(15,50,100,0,0,180)
 motor2 = Motor(8,40,20,0,30,30)
 motor3 = Motor(7,120,165,140,160,150)
 motor5 = Motor(5,160,110,0,110,110)
-motor6 = Motor(6,180,90,0,90,180)
+motor6 = Motor(6,180,70,0,70,180)
 
 motor_list=[motor1,motor2,motor3,motor5,motor6]
 
