a8b27eace55bd7746eeb57e8276776af7f33371f
shahzaibWaseem27 <110247209+shahzaibWaseem27@users.noreply.github.com>
2024-06-04 12:15:23 +0200

    Add files via upload

---
+++ Conveyor_belt/MQTT.h
@@ -0,0 +1,78 @@
+#ifndef MQTT_h
+#define MQTT_h
+#define PUBLISH_LED_PIN 6
+#include "Arduino.h"
+#include <ArduinoMqttClient.h>
+
+char broker[] = "192.168.162.44";
+int port = 1883;
+
+MqttClient mqttClient(wifiClient);
+
+void connectToMQTTBroker(){
+
+  if (!mqttClient.connect(broker, port)) {
+    while (1);
+  }
+
+  Serial.println("Connected to MQTT broker\n");
+
+}
+
+void publishMessage(String message, String topic){
+
+  digitalWrite(PUBLISH_LED_PIN, HIGH);
+  Serial.println("Publishing message ... \n");
+  Serial.print(message);
+  Serial.println();
+  
+  mqttClient.beginMessage(topic);
+  mqttClient.print(message);
+  mqttClient.endMessage();
+
+  Serial.println("Published message \n");
+  Serial.print(message);
+  Serial.println();
+  digitalWrite(PUBLISH_LED_PIN, LOW);
+
+}
+
+void subscribeToTopic(String subscribingTopic){
+
+  Serial.print("Subscribing to topic: ");
+  Serial.println(subscribingTopic);
+  Serial.println();
+
+  // subscribe to a topic
+  mqttClient.subscribe(subscribingTopic);
+
+  Serial.println("Subscribed to topic: \n");
+  Serial.println(subscribingTopic);
+  Serial.println();
+
+}
+
+void onMqttMessage(int messageSize) {
+  // we received a message, print out the topic and contents
+  Serial.println("Received a message with topic '");
+  Serial.print(mqttClient.messageTopic());
+  Serial.print("', length ");
+  Serial.print(messageSize);
+  Serial.println(" bytes:");
+
+  char message;
+
+  // use the Stream interface to print the contents
+  while (mqttClient.available()) {
+    message = (char)mqttClient.read();
+  }
+
+  if(message == "1"){
+    canContinue = 0;
+  } else {
+    canContinue = 1;
+  }
+
+}
+
+#endif
--- Conveyor_belt/main.ino
+++ Conveyor_belt/main.ino
@@ -1,105 +1,59 @@
-#include <ArduinoMqttClient.h>
-#include <WiFiNINA.h>
+#include "WLAN.h"
+#include "MQTT.h"
 
-///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "Galaxy";        // your network SSID
-char pass[] = "andrew2020";    // your network password
+#define stepPin 2
+#define dirPin 5 
+#define WLAN_CONNECTED_LED_PIN 7
+#define SUBSCRIBE_TO_COLOR_SENSOR_LED_PIN 5
 
-WiFiClient wifiClient;
-MqttClient mqttClient(wifiClient);
-
-const char broker[] = "192.168.17.44";
-int        port     = 1883;
-const char topic[]  = "ColorSensorReading";
+unsigned int canContinue = 1;
 
 void setup() {
-  //Initialize serial and wait for port to open:
+  
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  // attempt to connect to Wifi network:
-  Serial.print("Attempting to connect to SSID: ");
-  Serial.println(ssid);
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
-    // failed, retry
-    Serial.print(".");
-    delay(5000);
-  }
-
-  Serial.println("You're connected to the network");
-  Serial.println();
-
-  Serial.print("Attempting to connect to the MQTT broker: ");
-  Serial.println(broker);
-
-  if (!mqttClient.connect(broker, port)) {
-    Serial.print("MQTT connection failed! Error code = ");
-    Serial.println(mqttClient.connectError());
-
-    while (1);
-  }
-
-  Serial.println("You're connected to the MQTT broker!");
-  Serial.println();
-
-  // set the message receive callback
-  mqttClient.onMessage(onMqttMessage);
-
-  Serial.print("Subscribing to topic: ");
-  Serial.println(topic);
-  Serial.println();
-
-  // subscribe to a topic
-  mqttClient.subscribe(topic);
-
-  // topics can be unsubscribed using:
-  // mqttClient.unsubscribe(topic);
-
-  Serial.print("Topic: ");
-  Serial.println(topic);
-
-  Serial.println();
-}
-
-void loop() {
-  // call poll() regularly to allow the library to receive MQTT messages and
-  // send MQTT keep alive which avoids being disconnected by the broker
-  mqttClient.poll();
-}
-
-void onMqttMessage(int messageSize) {
-  // we received a message, print out the topic and contents
-  Serial.println("Received a message with topic '");
-  Serial.print(mqttClient.messageTopic());
-  Serial.print("', length ");
-  Serial.print(messageSize);
-  Serial.println(" bytes:");
-
-  // use the Stream interface to print the contents
-  while (mqttClient.available()) {
-    Serial.print((char)mqttClient.read());
-  }
-  Serial.println();
-  Serial.println();
-}
-
-// defines pins
-/*
-#define stepPin 2
-#define dirPin 5 
-void setup() {
-  // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
   digitalWrite(dirPin,HIGH);
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(WLAN_CONNECTED_LED_PIN, OUTPUT);
+  pinMode(SUBSCRIBE_TO_COLOR_SENSOR_LED_PIN, OUTPUT);
+  
+  connectToWifi();
+
+  connectToMQTTBroker();
+
+  subscribeToTopic("DistanceSensorReading");
+
 }
+
+
 void loop() {
-  digitalWrite(dirPin,HIGH);
+
+  if(!mqttClient.connected()){
+
+    digitalWrite(LED_BUILTIN, LOW);
+
+    connectToWifi();
+
+    connectToMQTTBroker();
+
+  } else if(canContinue){
+
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    digitalWrite(dirPin,HIGH);
 
     digitalWrite(stepPin,HIGH); 
     delayMicroseconds(400);    // by changing this time delay between the steps we can change the rotation speed
     digitalWrite(stepPin,LOW); 
     delayMicroseconds(400); 
+    
+  }
+
+  delay(100); 
+
 }
-*/
